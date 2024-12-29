497f854ee8f6dbda96c13a0bec546f2fd4027570
Poonam <96908995+poonam2003@users.noreply.github.com>
2024-12-29 17:59:10 +0530

    Add files via upload

---
+++ Introduction of SQL.sql
@@ -0,0 +1,142 @@
+# Job Profiles
+
+-- Data Analyst
+-- Business Analyst
+-- MIS Analyst
+-- Power BI Developer
+-- Data Engineer
+-- Data Scientist
+
+#-----------------------------------------------------------------------------------------------------------------------------------
+
+# Skills
+-- SQL, Excel, Power Bi, Tableau, Python, Machine Learning
+
+#-----------------------------------------------------------------------------------------------------------------------------------
+
+# Steps :- 
+
+# 1. Problem Understanding :- 
+# 2. Data Collection :- SQL (1-2 Days), Python(10 Days), Excel or CSV
+# 3. Data Cleaning :- Excel, SQL, Power Bi, Python
+-- 		40% to 60% work
+# 4. Data Visualization :- Power BI, Tableau
+# 5. Reports
+
+#-----------------------------------------------------------------------------------------------------------------------------------
+
+-- SQL :- Data Collection, Data Cleaning
+-- Excel :- Data Cleaning, Data Visualization
+-- Python :- Data Collection, Data Cleaning, Data Visualization
+-- Power BI :- Data Cleaning, Data Visualization
+-- Tableau :- Data Visualization
+
+#------------------------------------------------------------------------------------------------------------------------------------
+
+# SQL :- SQL stands for Structured Query Language
+-- 		which is used to access and manipulate the database or table. 
+
+-- It is a database language.
+-- It is also called programming language. 
+
+# Database :- Database is a collection of tables.
+# Table :- It is a collection of rows and columns.
+
+# MYSQL Vs SQL
+
+# MYSQL :- MYSQL is a software thats inside we use SQL language.
+-- 		   It is a RDBMS.
+
+# RDBMS :- Relational Database Management System
+-- Its manage the database.
+
+# DBMS :- Database Management System
+-- Its manage the table from database.
+
+#------------------------------------------------------------------------------------------------------------------------------------
+
+# SQL Commands
+
+-- There are five SQL Commands. 
+
+# 1. DDL :- Data Definition Language
+-- 			It is used to create the structure of database or table.
+
+# 2. DML :- Data Manipulation Language
+-- 			It is used to manipulate the table.
+
+# 3. DCL :- Data Control Language
+
+# 4. TCL :- Transaction Control Language
+
+# 5. DQL :- Data Query Language
+
+#-----------------------------------------------------------------------------------------------------------------------------------
+
+# DDL :- Data Definition Language
+
+# 1. CREATE :- 	It is used to create a table or a database.
+# 2. DROP :- 	It is used to delete the database or table.
+# 3. ALTER :- 	It is used to add a column in a table. 
+-- 				It is used to delete a column from the table.
+-- 				Its also rename the headers of column. 
+-- 				It is used to change the datatype of a column. 
+# 4. TRUNCATE :- Truncate are used to delete the data from a table and keep the format. 
+
+#------------------------------------------------------------------------------------------------------------------------------------
+
+# DML :- Data Manipulation Language
+
+# 1. INSERT :- With the help of this command we can add values inn particular rows. 
+# 2. UPDATE :- It is used to add the value or replace the value in a particular cell.
+# 3. DELETE :- Delete are used to delete the rows from table according to user. 
+
+#------------------------------------------------------------------------------------------------------------------------------------
+
+# DCL :- Data Control Langugae
+
+# 1. GRANT :- It is used to give the permission to other user to manipulate the data. 
+# 2. REVOKE :- Its restrict the permission. 
+
+#-------------------------------------------------------------------------------------------------------------------------------------
+
+# TCL :- Transaction Control Language
+
+# 1. SAVEPOINT :- Its save the data temporary base in database. 
+# 2. ROLLBACK :- Its take a step back in data. 
+# 3. COMMIT :- Its save the data permamently in database.
+
+#-------------------------------------------------------------------------------------------------------------------------------------
+
+# DQL :- Data Query Language
+
+# 1. SELECT :- Its retrive the data from the database or retrive the database from the cloud. 
+
+#-------------------------------------------------------------------------------------------------------------------------------------
+
+# Data :- Data is the collection of raw facts and figures.
+# Information :- Information is the collection of data. 
+
+#-------------------------------------------------------------------------------------------------------------------------------------
+
+# Data Type :- Its show the nature of the data or column. 
+
+# 1. Numerical Data :- 1. Integer,    2. Float,    3. Bigint
+# 2. Text Data :- 1. Char(255),    2. Varchar(3000),    3. Text
+# 3. Date and Time :- 1. Date,    2. Time,    3. Date&Time
+-- 		Format of date :- "YYYY-MM-DD"
+# 4. Boolean Data :- 1. True ,    2. False
+
+#-------------------------------------------------------------------------------------------------------------------------------------
+
+# Operators :- 
+
+# 1. Arithmetic Operators :- [+,  -,  *,  /,  %]
+# 2. Comparision Operators :- [>,  <,  >=,  <=, =, != or <>]
+# 3. Logical Operators :- [And, Or, Between, Like]
+# 4. Membership Operators :- [In, Not In]
+# 5. Bitwise Operators :- [&, |, ^]
+
+
+
+
